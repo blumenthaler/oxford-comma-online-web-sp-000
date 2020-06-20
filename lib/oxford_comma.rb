37de7c138451735c6b65ec01d1ae46008319b0e2
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-20 19:40:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   length = array.length
-  if length = 1
+  if length == 1
     "#{array[0]}"
-  elsif length = 2 
+  elsif length == 2 
     array_2 = array.join(" and ")
     array_2
-  elsif length = 3
+  elsif length == 3
     array_3 = array.join(" , ")
     array_3
   end
