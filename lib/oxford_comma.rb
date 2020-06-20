c75df7f22d71dc76550f2c765cb9ac4bc79b5ff5
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-20 19:13:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,12 @@
 def oxford_comma(array)
- # if array has 1 element
-  new_array = array.join
-  new_array
-  
-# if array has 2 elements:
-# array_2 = array.join(" and ")
-# array_2
-
-# if array has 3 elements:
-# array_3 = array.join(" , ")
-# array_3
-  #end
+  if array.length = 1
+    array_1 = array.join
+    array_1
+  elsif array.length = 2 
+    array_2 = array.join(" and ")
+    array_2
+  elsif array.length = 3
+    array_3 = array.join(" , ")
+    array_3
+  end
 end
